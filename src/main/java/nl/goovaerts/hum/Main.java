ca1741a68aa764bf9ea6a65a1a84923a8f2dc928
Caroline Goovaerts <caroline@goovaerts.nl>
2018-01-02 12:36:44 +0100

    Add project skeleton

---
+++ src/main/java/nl/goovaerts/hum/Main.java
@@ -0,0 +1,8 @@
+package nl.goovaerts.hum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ...
+    }
+}
---
+++ src/test/java/nl/goovaerts/hum/MainTest.java
@@ -0,0 +1,5 @@
+package nl.goovaerts.hum;
+
+public class MainTest {
+
+}
