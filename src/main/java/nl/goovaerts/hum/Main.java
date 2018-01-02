11c7d467dfc1c9672d690de9f704cc91ac0acbc0
Caroline Goovaerts <caroline@goovaerts.nl>
2018-01-02 16:13:47 +0100

    Add Spring Boot aspects

--- src/main/java/nl/goovaerts/hum/Main.java
+++
@@ -1,5 +0,0 @@
-package nl.goovaerts.hum;
-
-public class Main {
-
-}
---
+++ src/test/java/nl/goovaerts/hum/ApplicationIT.java
@@ -0,0 +1,11 @@
+package nl.goovaerts.hum;
+
+import org.junit.Test;
+
+// @RunWith Spring?
+public class ApplicationIT {
+
+    @Test
+    public void x() {
+    }
+}
